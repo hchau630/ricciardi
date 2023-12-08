082789e4dd5583109fbb677a993e2337b4428132
Ho Yin Chau <hoyinchau@Hos-MacBook-Pro-4.local>
2023-12-08 16:13:32 -0500

    slight optimizations and included new benchmarks

--- benchmark.py
+++ benchmark.py
@@ -3,25 +3,34 @@ import argparse
 
 import torch
 
+from ricciardi import dawsn, erfi, ierfcx, ricciardi
 
-def poly1d(coef, x):
-    power = torch.arange(len(coef), device=x.device)
-    return (coef * x[:, None]**power).sum(dim=-1)
+
+def format_time(t):
+    units = ['s', 'ms', 'μs', 'ns']
+    i = 0
+    while t < 1.0:
+        t = t * 1.0e3
+        i = i + 1
+    return f'{t:.3g} {units[i]}'
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', '-d', type=str, default='cpu')
+    parser.add_argument('-N', type=int, default='10001')
     args = parser.parse_args()
     device = torch.device(args.device)
     
-    c = torch.tensor([1.0, -0.5, 0.25, -0.125, 0.25, 0.2, -0.1, 0.05], device=device)
-    x = torch.linspace(-5.0, 5.0, 100001, device=device)
+    x = torch.linspace(-10.0, 10.0, args.N, device=device)
 
-    num, timing = timeit.Timer('poly1d(c, x)', globals={**locals(), **globals()}).autorange()
-    print(f'Parallel: {timing / num:.5e}s')
-    num, timing = timeit.Timer('c[0] + (c[1] + (c[2] + (c[3] + (c[4] + (c[5] + (c[6] + c[7] * x) * x) * x) * x) * x) * x) * x', globals={**locals(), **globals()}).autorange()
-    print(f'Sequential: {timing / num:.5e}s')
+    for name, func in {'dawsn': dawsn, 'erfi': erfi, 'ierfcx': ierfcx}.items():
+        num, timing = timeit.Timer('func(x)', globals={**locals(), **globals()}).autorange()
+        print(f'{name}: {format_time(timing / num)}')
+
+    x = torch.linspace(-0.08, 1.0, args.N, device=device)
+    num, timing = timeit.Timer('ricciardi(x)', globals={**locals(), **globals()}).autorange()
+    print(f'ricciardi: {format_time(timing / num)}')
 
 
 if __name__ == '__main__':
